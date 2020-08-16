428376b9df6ab2b58af72d7e1702541018070c70
Sheridan Beckwith Green <sheridan.green@yale.edu>
2020-08-16 13:11:49 -0400

    added the 2007scape scraper script

--- scraper.py
+++ scraper.py
@@ -1,83 +1,92 @@
-import math
-import json
 import requests
-import itertools
-import numpy as np
-
+import json
+import re
 import time
-from datetime import datetime, timedelta
+import math
+from datetime import datetime
+import pandas as pd
+from os import path
 
-# based on:
-# https://medium.com/@pasdan/how-to-scrap-reddit-using-pushshift-io-via-python-a3ebcc9b83f4
+# slightly edited version of code presented here:
+# https://www.osrsbox.com/blog/2019/03/18/watercooler-scraping-an-entire-subreddit-2007scape/
 
-def make_request(uri, max_retries = 5):
-    def fire_away(uri):
-        response = requests.get(uri)
-        assert response.status_code == 200
-        return json.loads(response.content)
-    current_tries = 1
-    while current_tries < max_retries:
-        try:
-            time.sleep(1)
-            response = fire_away(uri)
-            return response
-        except:
-            time.sleep(1)
-            current_tries += 1
-    return fire_away(uri)
+PUSHSHIFT_REDDIT_URL = "https://api.pushshift.io/reddit"
 
-def get_selftext(post):
-    try:
-        text = post['selftext']
-    except KeyError:
-        text = ''
-    return text
+def fetchObjects(**kwargs):
+    # Default paramaters for API query
+    params = {
+        "sort_type":"created_utc",
+        "sort":"asc",
+        "size":100
+        }
 
-def pull_posts_for(subreddit, start_at, end_at):
+    # Add additional paramters based on function arguments
+    for key,value in kwargs.items():
+        params[key] = value
+
+    # Print API query paramaters
+    print(params)
+
+    # Set the type variable based on function input
+    # The type can be "comment" or "submission", default is "comment"
+    type = "comment"
+    if 'type' in kwargs and kwargs['type'].lower() == "submission":
+        type = "submission"
     
-    def map_posts(posts):
-        return list(map(lambda post: {
-            'id': post['id'],
-            'created_utc': post['created_utc'],
-            'prefix': 't4_',
-            'title': post['title'],
-            'text': get_selftext(post)
-        }, posts))
-    
-    SIZE = 100
-    URI_TEMPLATE = r'https://api.pushshift.io/reddit/search/submission?subreddit={}&after={}&before={}&size={}'
+    # Perform an API request
+    r = requests.get(PUSHSHIFT_REDDIT_URL + "/search/" + type, params=params)
+    print(r)
 
-    post_collections = map_posts( \
-        make_request( \
-            URI_TEMPLATE.format( \
-                subreddit, start_at, end_at, SIZE))['data'])
-    n = len(post_collections)
-    while n == SIZE:
-        last = post_collections[-1]
-        new_start_at = last['created_utc'] - (10)
+    # Check the status code, if successful, process the data
+    if r.status_code == 200:
+        response = json.loads(r.text)
+        data = response['data']
+        sorted_data_by_id = sorted(data, key=lambda x: int(x['id'],36))
+        return sorted_data_by_id
+
+def extract_reddit_data(**kwargs):
+    # Specify the start timestamp
+    max_created_utc = math.floor(datetime(2010,1,1).timestamp())
+    max_id = 0
+
+    # Open a file for JSON output
+    fn = kwargs['subreddit'] + "_" + kwargs['type'] + ".json"
+    if(path.exists(fn)):
+        existing_data = pd.read_json(fn, lines=True)
+        most_recent = existing_data['created_utc'].idxmax()
+        recent_created_utc = existing_data['created_utc'][most_recent]
+        recent_id = int(existing_data['id'][most_recent],36)
+        if(recent_created_utc > max_created_utc):
+            max_created_utc = recent_created_utc - 1
+            max_id = recent_id
+    file = open(fn,"a")
+
+
+    # While loop for recursive function
+    while 1:
+        nothing_processed = True
+        # Call the recursive function
+        objects = fetchObjects(**kwargs,after=max_created_utc)
         
-        more_posts = map_posts( \
-            make_request( \
-                URI_TEMPLATE.format( \
-                    subreddit, new_start_at, end_at, SIZE))['data'])
+        # Loop the returned data, ordered by date
+        for object in objects:
+            id = int(object['id'],36)
+            if id > max_id:
+                nothing_processed = False
+                created_utc = object['created_utc']
+                max_id = id
+                if created_utc > max_created_utc: max_created_utc = created_utc
+                # Output JSON data to the opened file
+                print(json.dumps(object,sort_keys=True,ensure_ascii=True),file=file)
         
-        n = len(more_posts)
-        post_collections.extend(more_posts)
-    return post_collections
+        # Exit if nothing happened
+        if nothing_processed: return
+        max_created_utc -= 1
 
+        # Sleep a little before the next recursive function call
+        time.sleep(1)
 
-subreddit = 'wallstreetbets'
-end_at = math.ceil(datetime.utcnow().timestamp())
-start_at = math.floor(datetime(2020,8,9).timestamp())
-posts = pull_posts_for(subreddit, start_at, end_at)
-## ~ 4314
-print(len(posts))
-## ~ 4306
-print(len(np.unique([ post['id'] for post in posts ])))
-
-
-for i in range(0,10):
-    print(posts[i])
-
-# Sample API request to get keys:
-#https://api.pushshift.io/reddit/search/submission?subreddit=wallstreetbets&after=1565546826&before=1597082827&size=200
+# Start program by calling function with:
+# 1) Subreddit specified
+# 2) The type of data required (comment or submission)
+extract_reddit_data(subreddit="wallstreetbets",type="submission")
